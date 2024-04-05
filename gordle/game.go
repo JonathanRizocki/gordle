f613a4ea17cf0e65b61fec3bbc20833ae296aa43
JonathanRizocki <j.rizocki@gmail.com>
2024-04-05 15:43:30 -0700

    Process user input tests.

--- gordle/game.go
+++ gordle/game.go
@@ -1,13 +1,24 @@
 package gordle
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+const solutionLength = 5
 
 // Game holds all the information we need to play a game of gordle
-type Game struct{}
+type Game struct {
+	reader *bufio.Reader
+}
 
 // New returns a Game, which can be used to play!
-func New() *Game {
-	g := &Game{}
+func New(playerInput io.Reader) *Game {
+	g := &Game{
+		reader: bufio.NewReader(playerInput),
+	}
 
 	return g
 }
@@ -16,4 +27,27 @@ func New() *Game {
 func (g *Game) Play() {
 	fmt.Println("Welcome to Gordle!")
 	fmt.Printf("Enter a guess:\n")
+}
+
+// ask reads input until a valid suggestion is made & returned
+func (g *Game) ask() []rune {
+	fmt.Printf("Enter a %d-character guess:\n", solutionLength)
+
+	for {
+		playerInput, _, err := g.reader.ReadLine()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr,
+				"Gordle failed to read your guess: %s\n", err.Error())
+		}
+
+		guess := []rune(string(playerInput))
+
+		if len(guess) != solutionLength {
+			_, _ = fmt.Fprintf(os.Stderr,
+				"Your attempt is invalid with Gordle's solution!"+
+					"Expected %d characters, got %d.\n", solutionLength, len(guess))
+		} else {
+			return guess
+		}
+	}
 }
