1f924fe4e246c495c7d5f9b8fe7666342ac14821
JonathanRizocki <j.rizocki@gmail.com>
2024-04-08 18:34:16 -0700

    Randomy word selected from english corpus to play game.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"learngo-pockets/gordle/gordle"
 	"os"
 )
@@ -8,8 +10,17 @@ import (
 const maxAttempts = 6
 
 func main() {
+	corpus, err := gordle.ReadCorpus("corpus/english.txt")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to read corpus: %s", err)
+		return
+	}
 
-	solution := "hello"
-	g := gordle.New(os.Stdin, solution, maxAttempts)
+	// Create the game
+	g, err := gordle.New(bufio.NewReader(os.Stdin), corpus, maxAttempts)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to start game :%s", err)
+		return
+	}
 	g.Play()
 }
